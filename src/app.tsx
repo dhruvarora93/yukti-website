e723ca0ba3654a295b5200118334f37b244e56b3
Dhruv Arora <dhruv.arora@asu.edu>
2026-05-31 18:51:36 -0700

    Fix FOUC by moving Google Fonts from CSS @import to preconnect link tags

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider, Title, Link } from "@solidjs/meta";
 import { Router, useLocation } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Show, Suspense } from "solid-js";
@@ -13,6 +13,9 @@ export default function App() {
         return (
           <MetaProvider>
             <Title>Yukti</Title>
+            <Link rel="preconnect" href="https://fonts.googleapis.com" />
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+            <Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=DM+Sans:wght@400;500;700&display=swap" />
             <Show when={!isAdmin()}>
               <header class="site-header">
                 <a href="/" class="site-logo">
